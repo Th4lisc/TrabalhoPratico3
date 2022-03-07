462365d89eb1ea3c2cfecc6e767f096d3c07f31e
Th4lisc <thaliscezar@gmail.com>
2022-03-07 12:29:48 -0300

    First commit

--- TP3/src/tp3/Estacao.java
+++ TP3/src/tp3/Estacao.java
@@ -1,5 +1,56 @@
 package tp3;
 
 public class Estacao {
+	
+	// Atributos
+	private int id;
+	private String nome;
+	private Endereco endereco;
+	private int numVagas;
+	private int numVagasDisponiveis;
+	
+	// Construtor
+	public Estacao(String nome, Endereco endereco, int numVagas, int numVagasDisponiveis) {
+		this.nome = nome;
+		this.endereco = endereco;
+		this.numVagas = numVagas;
+		this.numVagasDisponiveis = numVagasDisponiveis;
+	}
+	
+	// Metodos
+	public String nome() {
+		return this.nome;
+	}
+	
+	public String endereco() {
+		return String.valueOf(this.endereco);
+	}
+	
+	public String numVagas() {
+		return String.valueOf(this.numVagas);
+	}
+	
+	public String numVagasDisponiveis() {
+		return String.valueOf(this.numVagasDisponiveis);
+	}
+	
+	public boolean editaEstacao(String nome, Endereco endereco, int numVagas, int numVagasDisponiveis) {
+		this.nome = nome;
+		this.endereco = endereco;
+		this.numVagas = numVagas;
+		this.numVagasDisponiveis = numVagasDisponiveis;
+		
+		return true;
+	}
+	
+	public boolean editaEstacao(int numVagasDisponiveis) {
+		this.numVagasDisponiveis = numVagasDisponiveis;
+		
+		return true;
+	}
+	
+	public boolean deletaEstacao() {
+		return true;
+	}
 
 }
